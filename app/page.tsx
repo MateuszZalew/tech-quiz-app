507112f1af3e99a1a695a18c986181fd3ef5da04
zalesiu <zalewmat31@gmail.com>
2024-08-20 22:59:43 +0200

    feat: add homepage view

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import QuizSettings from "@/components/quiz-settings";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="pt-32">
+      <h1 className="text-4xl text-center">
+        The lead gen + marketing checklist for business coaches
+      </h1>
+      <p className="text-center my-4">
+        You&apos;re working in one of the most competitive industries in the
+        world. Is your marketing up to the challenge?
+      </p>
+      <QuizSettings />
+    </main>
+  );
 }
